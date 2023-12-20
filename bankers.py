631c97d99f0b48129579d2050238bef6bf1c6b27
animal-23 <148389990+animal-23@users.noreply.github.com>
2023-12-20 09:42:20 +0530

    Update bankers.py

--- bankers.py
+++ bankers.py
@@ -1,74 +1,41 @@
-procs = int(input('Enter the number of processes: '))
-resrs = list(map(int, input('Enter the total resources of system: ').split()))
-
-# procs = 5
-# resrs = [10,5,7]
+# n = 5
+# m = [10,5,7]
 
 # alloc = [[0,1,0],[2,0,0],[3,0,2],[2,1,1],[0,0,2]]
-# max_need = [[7,5,3],[3,2,2],[9,0,2],[4,2,2],[5,3,3]]
+# max = [[7,5,3],[3,2,2],[9,0,2],[4,2,2],[5,3,3]]
+# avail = [3,3,2]
 
-alloc = []
-max_need = []
-
-print('Enter the allocated resources in matrix form: ')
-for i in range(procs):
-    temp = list(map(int, input().split()))
-    alloc.append(temp)
-
-print('Enter the max need of resources in matrix form: ')    
-for i in range(procs):
-    temp = list(map(int, input().split()))
-    max_need.append(temp)
-
-def is_safe(procs,resrs,alloc,max_need):
-    avail = []
-    for i in range(len(resrs)):
-        l = 0
-        for j in range(procs):
-            l += alloc[j][i]
-        avail.append(l)
-
-    rem_need = []
-    for i in range(procs):
-        temp = []
-        for j in range(len(resrs)):
-            a = max_need[i][j]-alloc[i][j]
-            temp.append(a)
-        rem_need.append(temp)
-
-    print(avail)
-    print(rem_need)
-    f = [0]*procs
-    ans = [0]*procs
-    ind = 0
-
-    for k in range(procs):
-        f[k] = 0
-
-    for k in range(procs):
-        for i in range(procs):
-            if f[i] == 0:
-                flag = 0
-            for j in range(len(resrs)):
-                if rem_need[i][j] > avail[j]:
-                    flag = 1
-                    break
-            if flag == 0:
-                ans[ind] = i
-                ind += 1
-                for y in range(len(resrs)):
-                    avail[y] += alloc[i][y]   
-                f[i]=1
-    return ans
-
-
-ans = is_safe(procs,resrs,alloc,max_need)
-if ans:
-    print('Safe sequence')
-    for i in range(procs):
-        if (i == procs-1):
-            print(f'P{ans[i]}')
-        else:
-            print(f'P{ans[i]} ->',end="")
+def is_safe(processes, alloc, max, avail):
+    n = len(processes)
+    m = len(avail)
+    work = avail.copy()
+    finish = [False] * n
+    need = [[max[i][j] - alloc[i][j] for j in range(m)] for i in range(n)]
+    safe_sequence = []
+    while True:
+        found = False
+        for i in range(n):
+            if not finish[i]:
+                if all(need[i][j] <= work[j] for j in range(m)):
+                    for j in range(m):
+                        work[j] += alloc[i][j]
+                    finish[i] = True
+                    safe_sequence.append(processes[i])
+                    found = True
+        if not found:
+            break
+    return all(finish), safe_sequence
+n = int(input("Enter the number of processes: "))
+m = int(input("Enter the number of resources: "))
+processes = list(range(n))
+print("Enter the allocation matrix:")
+alloc = [list(map(int, input().strip().split())) for _ in range(n)]
+print("Enter the max matrix:")
+max = [list(map(int, input().strip().split())) for _ in range(n)]
+avail = list(map(int, input("Enter the available resources: ").strip().split()))
+safe_state, sequence = is_safe(processes, alloc, max, avail)
+if safe_state:
+    print("The system is in a safe state.")
+    print("Safe Sequence:", " -> ".join(f"P{p+1}" for p in sequence))
 else:
-    print("The system is not in a safe state.")
+    print("The system is NOT in a safe state.")
